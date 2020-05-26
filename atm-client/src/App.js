749b3a14d8a9626f611807e52ab832529a6508ea
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-26 15:21:13 -0500

    start

--- atm-client/src/App.js
+++ atm-client/src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FirstComponent from './components/FirstComponent';
-import SecondComponent from './components/SecondComponent';
-import Counter from './components/counter/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter />
+        Hello World
       </div>
     )
   }
 }
 
-class LearningComponents extends Component {
-  render() {
-    return (
-      <div>
-        <FirstComponent></FirstComponent>
-        <SecondComponent></SecondComponent>
-      </div>
-    )
-  }
-}
 
 export default App;
