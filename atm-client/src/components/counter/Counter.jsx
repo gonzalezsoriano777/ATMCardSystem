c443ccea8bc04b6e6c4d7a93250648fb1ab8f15a
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-25 20:16:26 -0500

    Created a basic calculator to add and substract a number such like a counter, to familiarize myself with react

--- atm-client/src/components/counter/Counter.jsx
+++ atm-client/src/components/counter/Counter.jsx
@@ -14,23 +14,45 @@ class Counter extends Component {
     }
 
     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   render() {
     return (
       <div className="counter">
-        <CounterButton by={1} incrementMethod={this.increment} />
-        <CounterButton by={5} incrementMethod={this.increment} />
-        <CounterButton by={10} incrementMethod={this.increment} />
+        <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement} />
+        <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement} />
+        <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement} />
         <span className="count">{this.state.counter}</span>
+        <div>
+          <button className="reset" onClick={this.reset}>Reset</button>
+        </div>
       </div>
     )
   }
 
-  increment(by) {
+  reset() {
     this.setState({
-      counter: this.state.counter + by
-    })
+      counter: 0
+    }
+    )
+  }
+
+  increment(by) {
+    this.setState(
+      (prevState) => {
+        return { counter: prevState.counter + by }
+      }
+    );
+  }
+
+  decrement(by) {
+    this.setState(
+      (prevState) => {
+        return { counter: prevState.counter - by }
+      }
+    );
   }
 
 }
@@ -44,13 +66,15 @@ class CounterButton extends Component {
     }
 
     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
   render() {
     return (
       <div className="counter">
         <button onClick={this.increment}> +{this.props.by}</button>
-        <span className="count">{this.state.counter}</span>
+        <button onClick={this.decrement}> -{this.props.by}</button>
+        {/* <span className="count">{this.state.counter}</span> */}
       </div>
     )
   }
@@ -62,6 +86,14 @@ class CounterButton extends Component {
 
     this.props.incrementMethod(this.props.by);
 
+  }
+
+  decrement() {
+    this.setState({
+      counter: this.state.counter - this.props.by
+    });
+
+    this.props.decrementMethod(this.props.by);
   }
 }
 
