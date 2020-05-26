b99e811059d212ac0e63ed81cebcf1f4de80f6d4
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-26 17:39:06 -0500

    Merging the server side of this project with the client side to start working from there

---
+++ rest-web-services/src/main/java/com/atmCardSystem/rest/webservices/restwebservices/RestWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.atmCardSystem.rest.webservices.restwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebServicesApplication.class, args);
+	}
+
+}
---
+++ rest-web-services/src/test/java/com/atmCardSystem/rest/webservices/restwebservices/RestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmCardSystem.rest.webservices.restwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
