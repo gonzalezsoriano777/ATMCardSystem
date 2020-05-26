349e81892fd0ac37f2f75b797797bd7fff03a92a
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-05-25 19:33:59 -0500

    Created a counter to add simple addition intergers and sum the amount based on the count

--- atm-client/src/components/counter/Counter.jsx
+++ atm-client/src/components/counter/Counter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Counter.css';
 
 //Define the initial state in a constructor
@@ -18,18 +19,63 @@ class Counter extends Component {
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+1</button>
+        <CounterButton by={1} incrementMethod={this.increment} />
+        <CounterButton by={5} incrementMethod={this.increment} />
+        <CounterButton by={10} incrementMethod={this.increment} />
         <span className="count">{this.state.counter}</span>
       </div>
     )
   }
 
-  increment() { // Update state - counter++
-    this.state.counter++;
+  increment(by) {
     this.setState({
-      counter: this.state.counter + 1
+      counter: this.state.counter + by
+    })
+  }
+
+}
+
+class CounterButton extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+
+    this.increment = this.increment.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment}> +{this.props.by}</button>
+        <span className="count">{this.state.counter}</span>
+      </div>
+    )
+  }
+
+  increment() {
+    this.setState({
+      counter: this.state.counter + this.props.by
     });
+
+    this.props.incrementMethod(this.props.by);
+
   }
 }
+
+
+// Default Counter when initializing its function
+/* Counter.defaultProps = {
+  by: 1
+}
+*/
+
+// checker for invalid inputs
+/* Counter.propTypes = {
+  by: PropTypes.number
+}
+*/
 
 export default Counter
