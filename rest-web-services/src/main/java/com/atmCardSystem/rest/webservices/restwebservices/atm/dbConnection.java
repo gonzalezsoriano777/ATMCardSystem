8239f2ff78d81cba2042f7425d7ce63982739f49
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-07-13 13:12:56 -0500

    File for DB connection created..

---
+++ rest-web-services/src/main/java/com/atmCardSystem/rest/webservices/restwebservices/atm/dbConnection.java
@@ -0,0 +1,6 @@
+package com.atmCardSystem.rest.webservices.restwebservices.atm;
+
+// will be used to grab the db connection
+public class dbConnection {
+
+}
---
+++ rest-web-services/src/main/java/com/atmCardSystem/rest/webservices/restwebservices/atm/package-info.java
@@ -0,0 +1,1 @@
+package com.atmCardSystem.rest.webservices.restwebservices.atm;
