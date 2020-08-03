0837163b2aa6ba6094129c7f1cd9b173a4b2dcd2
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-08-03 12:59:32 -0500

    Commenting them out for section continuation!!

--- rest-web-services/src/main/java/com/atmCardSystem/rest/webservices/restwebservices/atm/jdbcTest.java
+++ rest-web-services/src/main/java/com/atmCardSystem/rest/webservices/restwebservices/atm/jdbcTest.java
@@ -11,31 +11,31 @@ public class jdbcTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String connString = "jdbc:mysql://localhost:3306/atm_system";
-
-		// SQL Server Credentials
-		String user = "root";
-		String pass = "Hector346321";
-
-		try {
-
-			// initializing connection
-			System.out.println("Finding connection & validating " + connString);
-			Connection myConn = DriverManager.getConnection(connString, user, pass);
-			System.out.println(" Connection successful.. located and connected to " + connString);
-			
-			//Class.forName("");
-
-			// translating dummy data into db server
-			PreparedStatement tableData = myConn.prepareStatement(
-					"INSERT INTO dummytable (Account_ID,firstName, lastName, depositAmount, Account_Type ) VALUES ('1', 'kim', 'jensen', '60', 'checking')");
-			tableData.executeUpdate();
-			tableData.close();
-			myConn.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+//		String connString = "jdbc:mysql://localhost:3306/atm_system";
+//
+//		// SQL Server Credentials
+//		String user = "root";
+//		String pass = "Hector346321";
+//
+//		try {
+//
+//			// initializing connection
+//			System.out.println("Finding connection & validating " + connString);
+//			Connection myConn = DriverManager.getConnection(connString, user, pass);
+//			System.out.println(" Connection successful.. located and connected to " + connString);
+//			
+//			//Class.forName("");
+//
+//			// translating dummy data into db server
+//			PreparedStatement tableData = myConn.prepareStatement(
+//					"INSERT INTO dummytable (Account_ID,firstName, lastName, depositAmount, Account_Type ) VALUES ('1', 'kim', 'jensen', '60', 'checking')");
+//			tableData.executeUpdate();
+//			tableData.close();
+//			myConn.close();
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} 
 
 	}
 
