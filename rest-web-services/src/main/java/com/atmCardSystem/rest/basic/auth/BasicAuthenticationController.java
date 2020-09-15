1e74426a766a46d5bdf997cce19dc1d9b6af13ef
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-09-15 00:33:42 -0500

    debugging a welcome component issue when grabbing the route to a path-variable name

--- rest-web-services/src/main/java/com/atmCardSystem/rest/basic/auth/BasicAuthenticationController.java
+++ rest-web-services/src/main/java/com/atmCardSystem/rest/basic/auth/BasicAuthenticationController.java
@@ -1,19 +1,19 @@
-package com.atmCardSystem.rest.basic.auth;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-//Controller
-@RestController
-@CrossOrigin(origins="http://localhost:4200")
-public class BasicAuthenticationController {
-	
-	@GetMapping(path="/basicauth")
-	public AuthenticationBean helloWorldBean() {
-		
-		return new AuthenticationBean("You are authenticated");
-		
-	}
-	
-}
+//package com.atmCardSystem.rest.basic.auth;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+////Controller
+//@RestController
+//@CrossOrigin(origins="http://localhost:4200")
+//public class BasicAuthenticationController {
+//	
+//	@GetMapping(path="/basicauth")
+//	public AuthenticationBean helloWorldBean() {
+//		
+//		return new AuthenticationBean("You are authenticated");
+//		
+//	}
+//	
+//}
